c1bdaa79d97213d7c2e910e184a8a85992d7d19f
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-09-27 14:33:57 +0200

    checkboxes are working and updating the DB

--- script.js
+++ script.js
@@ -30,7 +30,51 @@ function display(items) {
 
     if (item.bought) {
       clone.querySelector(".bought").checked = true;
+      clone.querySelector("h3.name").classList.add("item_bought");
+    } else {
+      clone.querySelector(".bought").checked = false;
+      clone.querySelector("h3.name").classList.remove("item_bought");
     }
+
+    clone.querySelector(".bought").addEventListener("change", () => {
+      itemStatus(item._id);
+    });
     parent.appendChild(clone);
   });
+}
+
+function itemStatus(id) {
+  let status = document.querySelector(`li[data-itemid="${id}"] > div > input`)
+    .checked;
+  if (status) {
+    document
+      .querySelector(`li[data-itemid="${id}"] > div > h3.name`)
+      .classList.add("item_bought");
+  } else {
+    document
+      .querySelector(`li[data-itemid="${id}"] > div > h3.name`)
+      .classList.remove("item_bought");
+  }
+  boughtUpdate(id, status);
+}
+
+function boughtUpdate(id, status) {
+  console.log(id, status);
+
+  let data = {
+    bought: status
+  };
+  let postData = JSON.stringify(data);
+
+  fetch(link + "/" + id, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": key,
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(d => d.json())
+    .then(t => console.log(t));
 }
